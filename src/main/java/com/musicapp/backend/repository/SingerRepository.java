5201905342dfcb013a340ac2634f6fe0a5bad108
Gia Khánh <176597134+Zakhanhrog@users.noreply.github.com>
2025-08-08 14:21:15 +0700

    Change repo singer, services

--- src/main/java/com/musicapp/backend/repository/SingerRepository.java
+++ src/main/java/com/musicapp/backend/repository/SingerRepository.java
@@ -2,13 +2,13 @@ package com.musicapp.backend.repository;
 
 import com.musicapp.backend.dto.singer.SingerDto;
 import com.musicapp.backend.entity.Singer;
+import com.musicapp.backend.entity.Singer.SingerStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import com.musicapp.backend.entity.Singer.SingerStatus;
 
 import java.util.List;
 import java.util.Optional;
