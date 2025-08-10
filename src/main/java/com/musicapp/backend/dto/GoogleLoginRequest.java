b2c27e223ddfc7f92a81d1d0f9f598a435a73fd3
Gia Khánh <176597134+Zakhanhrog@users.noreply.github.com>
2025-08-10 11:06:09 +0700

    Add enviroment gg

--- src/main/java/com/musicapp/backend/dto/GoogleLoginRequest.java
+++ src/main/java/com/musicapp/backend/dto/GoogleLoginRequest.java
@@ -2,10 +2,12 @@ package com.musicapp.backend.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class GoogleLoginRequest {
 
-    @NotBlank(message = "Google ID Token không được để trống.")
+    @NotBlank(message = "Google ID token không được để trống.")
     private String idToken;
 }
