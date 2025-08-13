fcf2fdbde372697378d84393e3579de587b3716a
vantutr <tranvantu06042001@gmail.com>
2025-08-13 09:47:02 +0700

    update task8: xem danh sach nguoi dung trong he thong

--- src/main/java/com/musicapp/backend/repository/UserRepository.java
+++ src/main/java/com/musicapp/backend/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.musicapp.backend.repository;
 
 import com.musicapp.backend.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +13,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.email = :email")
     Optional<User> findByEmailWithRoles(@Param("email") String email);
+
+    Page<User> findByDisplayNameContainingIgnoreCaseOrEmailContainingIgnoreCase(String displayName, String email, Pageable pageable);
+
 
     Optional<User> findByEmail(String email);
     Optional<User> findByPhoneNumber(String phoneNumber);
